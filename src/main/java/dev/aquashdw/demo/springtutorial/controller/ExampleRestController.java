b23212e193fccb3f761dfc276cad738c980bf9a8
aquashdw <aquashdw@gmail.com>
2021-06-19 20:57:28 +0900

    set response headers

--- src/main/java/dev/aquashdw/demo/springtutorial/controller/ExampleRestController.java
+++ src/main/java/dev/aquashdw/demo/springtutorial/controller/ExampleRestController.java
@@ -3,10 +3,14 @@ package dev.aquashdw.demo.springtutorial.controller;
 import dev.aquashdw.demo.springtutorial.dto.ExampleDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("rest")
@@ -33,5 +37,29 @@ public class ExampleRestController {
         return new ExampleDto("hello world!");
     }
 
+    @RequestMapping(
+            value = "getWithHeadersServlet",
+            method = RequestMethod.GET
+    )
+    public ExampleDto getReturnHeaders(HttpServletResponse servletResponse){
+        logger.info("returning new headers");
+        servletResponse.setHeader("custom-response-header", "Hello world!");
+        servletResponse.setStatus(200);
+        return new ExampleDto("check headers with f12");
+    }
 
+    @RequestMapping(
+            value = "getWithHeadersEntity",
+            method = RequestMethod.GET
+    )
+    public ResponseEntity<ExampleDto> getReturnHeaderEntity(){
+        logger.info("returning new headers");
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("custom-response-header", "Hello world!");
+
+        return ResponseEntity
+                .status(200)
+                .headers(responseHeaders)
+                .body(new ExampleDto("check headers with f12"));
+    }
 }
