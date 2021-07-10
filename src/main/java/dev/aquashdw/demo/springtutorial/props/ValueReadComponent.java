257f6c3a7f8c0783b37e9ccc55b532d866238ed9
aquashdw <aquashdw@gmail.com>
2021-07-10 15:58:32 +0900

    spring profiles group

--- src/main/java/dev/aquashdw/demo/springtutorial/props/ValueReadComponent.java
+++ src/main/java/dev/aquashdw/demo/springtutorial/props/ValueReadComponent.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
-import java.util.List;
 
 @Component
 public class ValueReadComponent {
